7f4861eaa994f98b0bebcdf37db4a6f240be26c2
Joel Ross <joelross@uw.edu>
2017-02-22 22:45:32 -0800

    Reset to 'starter' code

--- index.ts
+++ index.ts
@@ -1,12 +1,4 @@
-import * as Filter from './filters'
+//import * as Filter from './filters'
 
 console.log("Running program...")
 
-//instantiate the class
-const upperFilter = new Filter.Upper();
-const bolderFilter = Filter.Bolder;
-
-process.stdin
-  .pipe(upperFilter)
-  .pipe(bolderFilter)
-  .pipe(process.stdout);
